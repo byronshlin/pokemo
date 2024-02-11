a70adfdb3d854eae7d83226898839df9a5c359c6
Byron Lin <byronlin@catchplay.com>
2024-02-11 12:39:29 +0800

    update library version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,16 +60,16 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-    implementation("com.github.bumptech.glide:glide:4.11.0")
-    implementation("com.github.bumptech.glide:okhttp3-integration:4.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.github.bumptech.glide:okhttp3-integration:4.16.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
-    kapt("com.github.bumptech.glide:compiler:4.11.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
 
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     debugImplementation("androidx.fragment:fragment-testing:$1.6.2")
 
 
@@ -83,7 +83,7 @@ dependencies {
 
 
 
-    val okhttp_version ="4.10.0"
+    val okhttp_version ="4.12.0"
     implementation("com.squareup.okhttp3:okhttp:${okhttp_version}")
     implementation("com.squareup.okhttp3:okhttp:${okhttp_version}")
     testImplementation("com.squareup.okhttp3:mockwebserver:${okhttp_version}")
@@ -91,7 +91,7 @@ dependencies {
 
     //API
     // CoRoutine
-    val coroutine_version ="1.6.4"
+    val coroutine_version ="1.7.3"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${coroutine_version}")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutine_version")
 
@@ -123,32 +123,32 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:4.10.3")
 
-    testImplementation("androidx.test.ext:junit:1.1.2")
-    testImplementation("androidx.test.ext:truth:1.3.0")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("androidx.test.ext:truth:1.5.0")
     testImplementation("com.google.truth:truth:1.1.3")
 
-    androidTestImplementation("androidx.test:core:1.0.0")
-    androidTestImplementation("androidx.test:runner:1.1.0")
-    androidTestImplementation("androidx.test:rules:1.1.0")
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
     // Optional -- UI testing with Espresso
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // Optional -- UI testing with UI Automator
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
     // Optional -- Hamcrest library
-    androidTestImplementation("org.hamcrest:hamcrest-library:1.3")
+    androidTestImplementation("org.hamcrest:hamcrest-library:2.2")
 
-    testImplementation("androidx.arch.core:core-testing:2.1.0")
-    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
 
     // Assertions
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.ext:truth:1.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.ext:truth:1.5.0")
     androidTestImplementation("com.google.truth:truth:1.1.3")
 
     //Mockito
-    testImplementation("org.mockito:mockito-core:3.2.0")
+    testImplementation("org.mockito:mockito-core:4.8.0")
     //Mockk for Kotlin
-    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("io.mockk:mockk:1.13.9")
 }
 
 
